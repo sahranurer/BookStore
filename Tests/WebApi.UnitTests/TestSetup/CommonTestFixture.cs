324ba0e60ad94fd9e2d9c1fd1837920042202650
sahranurer <sahranurerr@gmail.com>
2022-02-27 18:52:55 +0300

    Test İşlemlerine Başlandı

---
+++ Tests/WebApi.UnitTests/TestSetup/CommonTestFixture.cs
@@ -0,0 +1,15 @@
+using WebApi.DbOperations;
+using AutoMapper;
+
+
+namespace TestSetup
+{
+    public class CommonTestFixture{
+        public BookStoreDbContext Context {get;set;}
+        public IMapper Mapper {get;set;}
+        public CommonTestFixture(){
+            var options = new DBContextOptionsBuild<BookStoreDbContext>().UseInMemoryDatabase(databaseName:"BookStoreTestDB").options;
+           
+        }
+    }
+}
